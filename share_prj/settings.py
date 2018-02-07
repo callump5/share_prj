10012ad34b46f3f3c9fadbd49bce65887c44ae9d
callump5 <clpullinger@gmail.com>
2018-02-07 14:36:30 +0000

    deploy?

--- share_prj/settings.py
+++ share_prj/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '1r@%#0jeyk7q5eh&$2s4-^8lgx1sb+312x%mt(clfefi8whau7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    share-basildon.herokuapp.com
+]
 
 
 # Application definition
