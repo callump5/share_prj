6c7f20f8b804ac77e0e8e443ba56e690db742927
callump5 <clpullinger@gmail.com>
2018-02-08 17:23:23 +0000

    Connect to Dropbox for media files

--- share_prj/settings.py
+++ share_prj/settings.py
@@ -148,9 +148,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
