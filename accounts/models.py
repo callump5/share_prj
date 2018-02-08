76ccd4266dd5311059a8389e5a75ff847daff3e3
callump5 <clpullinger@gmail.com>
2018-02-08 16:12:16 +0000

    Connect to Dropbox for media files

--- accounts/models.py
+++ accounts/models.py
@@ -2,20 +2,3 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.contrib.auth.models import User
-from tinymce.models import HTMLField
-
-from share_prj.dropbox_upload_handler import upload_file
-
-# Create your models here.
-
-class Staff_Details(models.Model):
-    user = models.ForeignKey(User, related_name='staff_profile')
-    bio = HTMLField()
-    staff_image = models.ImageField(upload_to='images', blank=True, null=True)
-
-    def __unicode__(self):
-        return self.user.username
-
-    upload_file()
-
