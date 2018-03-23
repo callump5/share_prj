f3f176df086d9c79dd79fa1978dd87c6fd3f0a27
callump5 <clpullinger@gmail.com>
2018-03-23 00:38:33 +0000

    Update settings.py

--- share_prj/settings.py
+++ share_prj/settings.py
@@ -175,8 +175,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'clpullinger@gmail.com'
-EMAIL_HOST_PASSWORD = 'mokkie23'
 
 
 EMAIL_PORT = 587
